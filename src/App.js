afc3fca724709a3eb168bde92116e7f11df9d86b
jean <jean.tostivint@gmail.com>
2020-05-23 12:52:41 +0200

    > Added commentaries

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import ChatMessage from './ui/chat/molecules/ChatMessage'
 
-const author = {
+const author_0 = {
   nickname: "Guillaume",
   chatColor: "#F2994A",
   badges: ['prime', 'mod']
 }
 
+const author_1 = {
+  nickname: "Jean",
+  chatColor: "#bfe35c",
+  badges: ['turbo', 'verified']
+}
+
+const author_2 = {
+  nickname: "Christina",
+  chatColor: "#3140cb",
+  badges: ['turbo']
+}
+
+const author_3 = {
+  nickname: "Maurice",
+  chatColor: "#c42c61",
+  badges: []
+}
+
 export default function App() {
   return (
     <div className="App">
-      <ChatMessage author={author} text=" Aurelien il commence a se faire tard la stop ton jeu vidéo et arrete de crier dans la maison "/>
-      <ChatMessage author={author} text=" Aurelien il commence a se faire tard la stop ton jeu vidéo et arrete de crier dans la maison "/>
+      <ChatMessage author={author_0} text="Aurelien il commence a se faire tard la stop ton jeu vidéo et arrete de crier dans la maison "/>
+      <ChatMessage author={author_1} text="Ah ah ah"/>
+      <ChatMessage author={author_2} text="Pas de change Aurelien"/>
+      <ChatMessage author={author_3} text="Sympa ce jeu"/>
     </div>
   );
 }
